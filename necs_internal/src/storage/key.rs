50b2fec587910f46fdc686c7427a08610e29cec2
sandmuel <samuel@sandmuel.net>
2025-11-18 19:30:02 -0800

    Wrap NodeKey doc comment

--- necs_internal/src/storage/key.rs
+++ necs_internal/src/storage/key.rs
@@ -1,10 +1,15 @@
 use slotmap::new_key_type;
 
 new_key_type! {
-    /// A key used to uniquely identify a node's tuple and components in [`World`]. This key is always used with the [`NodeId`] or the [`ComponentId`] wrapper.
+    /// A key used to uniquely identify a node's tuple and components in
+    /// [`World`].
+    ///
+    /// This key is generally used with the [`NodeId`] or the [`ComponentId`]
+    /// wrapper, but may be used individually as a unique identifier for node
+    /// metadata.
     ///
     /// [`World`]: crate::World
     /// [`NodeId`]: crate::NodeId
     /// [`ComponentId`]: crate::ComponentId
-    pub(crate) struct NodeKey;
+    pub struct NodeKey;
 }
